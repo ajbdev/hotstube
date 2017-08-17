7589f48b85e2e41b96fa3e6d437799a73c88565a
Andy Baird <andy@threadsculture.com>
2017-08-17 17:25:15 -0500

    Timline

--- ui/Game/Highlights.js
+++ ui/Game/Highlights.js
@@ -1,5 +1,5 @@
 const React = require('react')
-const {Timeline,TimelineEvent} = require('./Timeline')
+const {Timeline,TimelineEvent,TimelineMarker} = require('./Timeline')
 
 
 class Highlights extends React.Component { 
@@ -9,7 +9,15 @@ class Highlights extends React.Component {
             <tab-content>
                 <div className="timeline">
                     <Timeline>
-                        <TimelineEvent></TimelineEvent>
+                        <TimelineEvent at="1:07" icon="spawn">
+                            Player 1 spawned
+                        </TimelineEvent>
+                        <TimelineEvent at="2:07" icon="firstblood">
+                            Player 2 gets first BLOOD
+                        </TimelineEvent>
+                        <TimelineMarker>
+                            10 Minutes
+                        </TimelineMarker>
                     </Timeline>
                 </div>
             </tab-content>
--- ui/Game/Timeline.js
+++ ui/Game/Timeline.js
@@ -13,10 +13,23 @@ class Timeline extends React.Component {
 class TimelineEvent extends React.Component { 
     render() {
         return (
-            <timeline-event>{this.props.children}</timeline-event>
+            <timeline-event>
+                <div className="time">{this.props.at}</div>
+                {this.props.children}
+            </timeline-event>
+        )
+    }
+}
+class TimelineMarker extends React.Component {
+    render() {
+        return (
+            <timeline-marker>
+                This is when shit got <i>weird</i>
+            </timeline-marker>
         )
     }
 }
 
 module.exports.Timeline = Timeline
 module.exports.TimelineEvent = TimelineEvent
+module.exports.TimelineMarker = TimelineMarker

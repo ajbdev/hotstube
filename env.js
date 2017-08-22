b099423bffe638adde74aa76f61f373806a26b8a
Andy Baird <andy@threadsculture.com>
2017-08-22 11:30:14 -0500

    Allow env var to be set for debugging

--- env.js
+++ env.js
@@ -1,1 +1,8 @@
-module.exports = /node_modules[\\/]electron[\\/]/.test(process.execPath) ? 'development' : 'production'
+
+const getFromEnv = parseInt(process.env.ELECTRON_IS_DEV, 10) === 1
+const isEnvSet = 'ELECTRON_IS_DEV' in process.env
+const isDev = isEnvSet ? getFromEnv : (process.defaultApp || /node_modules[\\/]electron[\\/]/.test(process.execPath))
+
+
+module.exports = isDev ? 'development' : 'production'
+

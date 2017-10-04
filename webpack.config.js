b676d2e5d9b8b4f52865b68d1b92716b1111d6b2
Andy Baird <andy@threadsculture.com>
2017-10-03 19:13:21 -0500

    Updated web versions

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'web/game')
+        path: path.resolve(__dirname, 'web/games')
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),

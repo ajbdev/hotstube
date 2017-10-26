28b51e220c8b9bcfcda581d183887e3452498fff
Andy Baird <andy@threadsculture.com>
2017-10-26 17:10:33 -0500

    Add config to rollbar payload

--- src/ui/bootstrap_electron.js
+++ src/ui/bootstrap_electron.js
@@ -3,7 +3,7 @@ const React = require('react')
 const App = require('./ui/App')
 const Rollbar = require('rollbar')
 const _env = require('./env').env
-
+const Config = require('../lib/Config')
 
 
 if (_env !== 'development') {
@@ -11,7 +11,10 @@ if (_env !== 'development') {
     accessToken: '5209cc3fb71f498190ecf601df11d98b',
     captureUncaught: true,
     captureUnhandledRejections: true,
-    environment: _env
+    environment: _env,
+    payload: {
+      config: Config.options
+    }
   })
 }
 
